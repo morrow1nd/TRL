23afa16e2d82848961f16a8ca9a8d6fd90d3de9a
morrow1nd <morrow1nd@yahoo.com>
2018-06-11 15:40:50 +0800

    build script

--- Source/CMakeSources.cmake
+++ Source/CMakeSources.cmake
@@ -1,16 +1,3 @@
-SET(TRL_INC_FILES
-    "${TRL_INCLUDE_DIR}/TRL/CommonType.h"
-    "${TRL_INCLUDE_DIR}/TRL/RenderAPI.h"
-    "${TRL_INCLUDE_DIR}/TRL/AttributeData.h"
-    "${TRL_INCLUDE_DIR}/TRL/GpuBuffer.h"
-    "${TRL_INCLUDE_DIR}/TRL/GpuProgram.h"
-    "${TRL_INCLUDE_DIR}/TRL/GpuShader.h"
-    "${TRL_INCLUDE_DIR}/TRL/TRLShader.h"
-    "${TRL_INCLUDE_DIR}/TRL/AttributeVariable.h"
-    "${TRL_INCLUDE_DIR}/TRL/UniformVariable.h"
-    "${TRL_INCLUDE_DIR}/TRL/GpuTexture.h"
-    "${TRL_INCLUDE_DIR}/TRL/GpuTexture2D.h"
-)
 
 SET(TRL_INC_SL
     "${TRL_INCLUDE_DIR}/TRL/TRLSL/DebugGenerator.h"
@@ -30,75 +17,64 @@ SET(TRL_SRC_SL
     "${TRL_SOURCE_DIR}/TRL/TRLSL/Token.cpp"
 )
 
+SET(TRL_INC_FILES
+    "${TRL_INCLUDE_DIR}/TRL/CommonType.h"
+    "${TRL_INCLUDE_DIR}/TRL/RenderAPI.h"
+    "${TRL_INCLUDE_DIR}/TRL/AttributeData.h"
+    "${TRL_INCLUDE_DIR}/TRL/GpuBuffer.h"
+    "${TRL_INCLUDE_DIR}/TRL/GpuProgram.h"
+    "${TRL_INCLUDE_DIR}/TRL/GpuShader.h"
+    "${TRL_INCLUDE_DIR}/TRL/TRLShader.h"
+    "${TRL_INCLUDE_DIR}/TRL/AttributeVariable.h"
+    "${TRL_INCLUDE_DIR}/TRL/UniformVariable.h"
+    "${TRL_INCLUDE_DIR}/TRL/GpuTexture.h"
+    "${TRL_INCLUDE_DIR}/TRL/GpuTexture2D.h"
+)
+
 SET(TRL_SRC_FILES
 	"${TRL_SOURCE_DIR}/TRL/TRLShader.cpp"
 )
 
-if(TRL_USING_OPENGL)
-	set(TRL_SRC_FILES
-		${TRL_SRC_FILES}
+set(TRL_SRC_OPENGL
+	"${TRL_SOURCE_DIR}/TRL/OpenGL/AttributeData.cpp"
+	"${TRL_SOURCE_DIR}/TRL/OpenGL/GpuBuffer.cpp"
+	"${TRL_SOURCE_DIR}/TRL/OpenGL/GpuProgram.cpp"
+	"${TRL_SOURCE_DIR}/TRL/OpenGL/GpuShader.cpp"
+	"${TRL_SOURCE_DIR}/TRL/OpenGL/AttributeVariable.cpp"
+	"${TRL_SOURCE_DIR}/TRL/OpenGL/UniformVariable.cpp"
+	"${TRL_SOURCE_DIR}/TRL/OpenGL/RenderAPI.cpp"
+	"${TRL_SOURCE_DIR}/TRL/OpenGL/GpuTexture.cpp"
+	"${TRL_SOURCE_DIR}/TRL/OpenGL/GpuTexture2D.cpp"
 
-		"${TRL_SOURCE_DIR}/OpenGL/AttributeData.cpp"
-		"${TRL_SOURCE_DIR}/OpenGL/GpuBuffer.cpp"
-		"${TRL_SOURCE_DIR}/OpenGL/GpuProgram.cpp"
-		"${TRL_SOURCE_DIR}/OpenGL/GpuShader.cpp"
-		"${TRL_SOURCE_DIR}/OpenGL/AttributeVariable.cpp"
-		"${TRL_SOURCE_DIR}/OpenGL/UniformVariable.cpp"
-		"${TRL_SOURCE_DIR}/OpenGL/RenderAPI.cpp"
-		"${TRL_SOURCE_DIR}/OpenGL/GpuTexture.cpp"
-		"${TRL_SOURCE_DIR}/OpenGL/GpuTexture2D.cpp"
+	"${TRL_SOURCE_DIR}/ThirdParty/GLAD/glad-c/src/glad.c"
+)
 
-		"${TRL_SOURCE_DIR}/ThirdParty/GLAD/glad-c/src/glad.c"
-	)
-endif()
-
-if(TRL_USING_OPENGLES)
-	set(TRL_SRC_FILES
-		${TRL_SRC_FILES}
-
+set(TRL_SRC_OPENGLES
 	
-	)
-endif()
+)
 
-if(TRL_USING_DX11)
-	set(TRL_SRC_FILES
-		${TRL_SRC_FILES}
+set(TRL_SRC_DX11
 
-	
-	)
-endif()
+)
 
-
-
-# if(MSVC)
-#   list(APPEND BS_BANSHEECORE_INC_PLATFORM VSVisualizations.natvis)
-# endif()
-
-# if(WIN32)
-#   list(APPEND BS_BANSHEECORE_INC_PLATFORM ${BS_BANSHEECORE_INC_PLATFORM_WIN32})
-#   list(APPEND BS_BANSHEECORE_SRC_PLATFORM ${BS_BANSHEECORE_SRC_PLATFORM_WIN32})
-# elseif(LINUX)
-#   list(APPEND BS_BANSHEECORE_INC_PLATFORM ${BS_BANSHEECORE_INC_PLATFORM_LINUX})
-#   list(APPEND BS_BANSHEECORE_SRC_PLATFORM ${BS_BANSHEECORE_SRC_PLATFORM_LINUX})
-# elseif(APPLE)
-#   list(APPEND BS_BANSHEECORE_INC_PLATFORM ${BS_BANSHEECORE_INC_PLATFORM_MACOS})
-#   list(APPEND BS_BANSHEECORE_SRC_PLATFORM ${BS_BANSHEECORE_SRC_PLATFORM_MACOS})
-# endif()
 
 source_group("CMake"									FILES "${TRL_SOURCE_DIR}/../CMakeLists.txt")
 source_group("CMake\\Source"                            FILES "CMakeSources.cmake" "CMakeLists.txt")
 source_group("CMake\\Source\\ThirdParty"				FILES "${TRL_SOURCE_DIR}/ThirdParty/CMakeLists.txt")
+
 source_group("Header Files"                             FILES ${TRL_INC_FILES})
-source_group("Source Files\\TRL_SL"                     FILES ${TRL_SRC_SL})
+source_group("Source Files"								FILES ${TRL_SRC_FILES})
 source_group("Header Files\\TRL_SL"						FILES ${TRL_INC_SL})
+source_group("Source Files\\TRL_SL"                     FILES ${TRL_SRC_SL})
+
 if(TRL_USING_OPENGL)
-source_group("Source Files\\OpenGL"						FILES ${TRL_SRC_FILES})
+source_group("Source Files\\OpenGL"						FILES ${TRL_SRC_OPENGL})
 endif()
 if(TRL_USING_OPENGLES)
-source_group("Source Files\\OpenGLES"					FILES ${TRL_SRC_FILES})
+source_group("Source Files\\OpenGLES"					FILES ${TRL_SRC_OPENGLES})
 endif()
 if(TRL_USING_DX11)
-source_group("Source Files\\DX11"						FILES ${TRL_SRC_FILES})
+source_group("Source Files\\DX11"						FILES ${TRL_SRC_DX11})
 endif()
 
 
@@ -111,3 +87,22 @@ set(TRL_SRC
     ${TRL_INC_SL}
 	${TRL_SRC_SL}
 )
+
+if(TRL_USING_OPENGL)
+	set(TRL_SRC
+		${TRL_SRC}
+		${TRL_SRC_OPENGL}
+	)
+endif()
+if(TRL_USING_OPENGLES)
+	set(TRL_SRC
+		${TRL_SRC}
+		${TRL_SRC_OPENGLES}
+	)
+endif()
+if(TRL_USING_DX11)
+	set(TRL_SRC
+		${TRL_SRC}
+		${TRL_SRC_DX11}
+	)
+endif()
